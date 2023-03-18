67c0d0743d92d390c04d375a09bdbf2ffa3e2907
Geek-Crown <adewaleayomide57@gmail.com>
2023-03-18 02:47:22 +0000

    Printing alphabets removig two alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - entry point, this is where the code starts
+ *
+ * Return: it returns the value 0
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == "q" || x == "e")
+			break;
+		putchar(x);
+		x++;
+	}
+		return (0);
+}
