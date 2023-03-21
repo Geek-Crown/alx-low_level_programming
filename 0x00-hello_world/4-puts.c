b152a447c8746dd2b8d50db3fdc4d2031195b3a5
Geek-Crown <adewaleayomide57@gmail.com>
2023-03-21 02:37:37 +0000

    Using Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#!/bin/bash
-#include stdio.h
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char a = "Programming is like building a multilingual puzzle\n";
-	puts(a);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
