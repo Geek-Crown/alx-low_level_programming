1a7838469ad81e4d6b1bcd46fbe41a725de1b030
Geek-Crown <adewaleayomide57@gmail.com>
2023-03-30 23:19:26 +0000

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies the src to the dest using n
+ * @dest: first string to overwrite
+ * @src: second string to copy
+ * @n: byte representation for src
+ *
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *str;
+
+	str = strncpy(dest, src, n)
+	return (str);
+}
