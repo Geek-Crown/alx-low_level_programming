d138b3b85a0a5ddcabddc7e75a19c0fda942eefb
Geek-Crown <adewaleayomide57@gmail.com>
2023-04-01 02:03:29 +0000

    Lower 2 Upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,22 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * string_toupper - all strings in uppercase
  * @'': the string
- * 
+ *
  * Return: string
  */
 char *string_toupper(char *)
 {
+	int len = strlen(str);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (islower(str[i]))
+		{
+			str[i] = toupper(str[i]);
+		}
+	}
+	return (str);
 }
