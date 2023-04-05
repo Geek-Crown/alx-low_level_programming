ede27bc4a161459d7873d1380adf779ba160b6b3
Geek-Crown <adewaleayomide57@gmail.com>
2023-04-05 04:10:02 +0000

    Modified main file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,6 @@ void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
+char *_strstr(char *haystack, char *needle);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strpbrk(char *s, char *accept);
