b2a9795cc0ba6c8f7e91d5a7781077bf3694db22
Geek-Crown <adewaleayomide57@gmail.com>
2023-04-05 04:06:31 +0000

    String Span

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
